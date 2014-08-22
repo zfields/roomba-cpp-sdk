b1a4d04812a64dde5ca77218cf1d24064714b901
Zachary J. Fields <zachary_fields@yahoo.com>
2014-08-21 20:04:47 -0700

    Latest documentation to reflect parameter changes  - driveDirect()  - drivePWM() Both will treat first parameter as left wheel instead of right

--- documentation/html/search/all_2.js
+++ documentation/html/search/all_2.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['digitledsascii',['digitLEDsASCII',['../classroomba_1_1series500_1_1_open_interface.html#a60f023e5d923f0cd66a5f69e0b094c8a',1,'roomba::series500::OpenInterface']]],
   ['digitledsraw',['digitLEDsRaw',['../classroomba_1_1series500_1_1_open_interface.html#a76c9b0291da94519b91ec5e6d9409838',1,'roomba::series500::OpenInterface']]],
-  ['drive',['drive',['../classroomba_1_1series500_1_1_open_interface.html#a0b02a89315ed3e6daa6b36b5f1327fc7',1,'roomba::series500::OpenInterface']]],
-  ['drivedirect',['driveDirect',['../classroomba_1_1series500_1_1_open_interface.html#a3148367bdbf04abcb590c48adc03d2a1',1,'roomba::series500::OpenInterface']]],
-  ['drivepwm',['drivePWM',['../classroomba_1_1series500_1_1_open_interface.html#aec2dbec11bbd03d2b07403692d43a658',1,'roomba::series500::OpenInterface']]]
+  ['drive',['drive',['../classroomba_1_1series500_1_1_open_interface.html#a54ef1ecc95d5df7aaa6646f0c1ce1bde',1,'roomba::series500::OpenInterface']]],
+  ['drivedirect',['driveDirect',['../classroomba_1_1series500_1_1_open_interface.html#aa528b5978d391829da67cc582beac9b9',1,'roomba::series500::OpenInterface']]],
+  ['drivepwm',['drivePWM',['../classroomba_1_1series500_1_1_open_interface.html#a57c4e739264089ea5982a2f17727d108',1,'roomba::series500::OpenInterface']]]
 ];
